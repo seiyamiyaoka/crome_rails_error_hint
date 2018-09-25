163c1847d206bbc038d285fe72cab928d7b6633d
seiyamiyaoka <seiyamiyaoka@gmail.com>
2018-09-25 20:05:45 +0900

    不要記述削除

--- run.js
+++
@@ -1,3 +0,0 @@
-import main from './testFunc';
-debugger
-main()
--- testFunc.js
+++ testFunc.js
@@ -31,31 +31,11 @@ class TestHint {
         return `オブジェクトが見つからないためエラーになっています。idで探している場合はそのidのデータが本当に存在しているか確認してみましょう`
       } else if (obj.title.indexOf("syntax error, unexpected keyword_ensure") >= 0) {
         return `do ~ endの部分で記述ミスをしている可能性があります。do~endのインデントを修正した上で数があっているか確認しましょう` 
+      } else if (obj.title.indexOf("uninitialized constant") >= 0)  {
+        return '定数の呼び出しに失敗しています。モデル名が間違っていないか確認しましょう。モデル自体が存在していない可能性もあります。'
       } else {
         return 'まだ対応できません。googleで検索をしましょう。'
       }
-      // switch (obj.title) {
-      //   case "undefined local variable or method":
-          
-      //     break;
-      //   case "syntax error, unexpected end-of-input, expecting keyword_end":
-      //     return `記述間違いをしている可能性があります。defまたはdoとendの数があっているか確認して見ましょう`
-      //     break
-      //   case "undefined method":
-      //     return `${obj.variable}メソッドが定義されていません。呼び出しもとのオブジェクトがメソッドを持っているか確認しましょう。\n
-      //     　　　　${obj.variable}メソッド名の入力間違いの可能性もあります。
-      //     `
-      //     break
-      //   case "syntax error, unexpected keyword_ensure, expecting end-of-input":
-      //     return `do ~ endの部分で記述ミスをしている可能性があります。do~endのインデントを修正した上で数があっているか確認しましょう`
-      //     break
-      //   case obj.title.indexOf("Couldn't find"):
-      //     debugger
-      //     return 
-      //   default:
-      //     return 'まだ設定できていません'
-      //     break;
-      // }
     }
     function hintBuilder() {
       let newSubClass = document.createElement('div')
@@ -75,10 +55,6 @@ class TestHint {
     newHtml.newH3.innerText = "エラー対処ヒント"
     newHtml.newTd.innerHTML = "エラー原因"
     newHtml.newPre.innerHTML = setAnswer(obj)
-    // format.querySelector('tbody').removeChild(format.querySelector('tbody').lastElementChild)
-    // format.querySelector("h3").innerText = "エラー対処ヒント"
-    // format.querySelector('td').innerText = "エラー原因"
-    // format.querySelector('pre').innerText = setAnswer(obj)
 
     newHtml.newSubClass.appendChild(newHtml.newH3)
     newHtml.newTd.appendChild(newHtml.newPre)
@@ -86,22 +62,9 @@ class TestHint {
 
     let parent = document.querySelector('.hint')
     if (!parent) {
-      parent =  document.querySelector('.sub') 
+      parent = document.querySelector('.sub') 
     }
-    
-    debugger
-    // if (parent.style) {
-    //   parent.style.display = 'block'
-    // } 
-    // const target = .variable_info
-    // debugger
-    // parent.innerHTML = newHtml.newSubClass
-    // parent.insertAdjacentHTML('afterbegin', newHtml.newSubClass);
     parent.parentNode.insertBefore(newHtml.newSubClass, parent);
-    // debugger
-    // parent.insertBefore(newHtml.newSubClass, parent.querySelector('.hint'))
-    // debugger
-    // parent.insertBefore(newHtml.newSubClass, parent.querySelector('.trace_info.clearfix').nextSibling)
   }
   removeNode(format) {
     return format
@@ -117,15 +80,5 @@ function main() {
   const test = new TestHint()
   test.createHintHtml(test.preprosessor_title(TestHint.changeTitle()))
 }
-// })
 
 main()
-
-// export function test() {
-//   console.log('test')
-// }
-
-// export {
-//   changeTitle
-// }
-  
